3df5f6d3d424f13858045f8a1cc87c2250be1153
lucy-rtzr <lucy@rtzr.ai>
2023-12-21 18:44:23 +0900

    project init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Rubik_Bubbles, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const rubikBubbles = Rubik_Bubbles({
+  subsets: ['latin'],
+  weight: ['400'],
+});
