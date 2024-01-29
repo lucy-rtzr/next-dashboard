1d1ac0f4b9b587b1b993e71bc9ca918ad9e18fde
lucy-rtzr <lucy@rtzr.ai>
2024-01-29 18:19:31 +0900

    area-label 추가 예시

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -125,7 +125,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           <div id="status-error" aria-live="polite" aria-atomic="true">
             {state.errors?.status &&
               state.errors.status.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
+                <p
+                  className="mt-2 text-sm text-red-500"
+                  key={error}
+                  aria-label={error}
+                >
                   {error}
                 </p>
               ))}
